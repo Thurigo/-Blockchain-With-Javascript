8e4a1bd233e8907f809e825888eb63547f179db6
Arthur <111078458+Thurigo@users.noreply.github.com>
2024-02-18 06:27:28 -0300

    Começando estudar Blockchain

---
+++ main.js
@@ -0,0 +1,70 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data ,previousHash = ''){
+        this.index = index
+        this.timestamp = timestamp
+        this.data = data
+        this.previousHash = previousHash
+        this.hash = this.calculaHash();
+    }
+
+    calculaHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+
+}
+
+class Blockchain {
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock(){
+        return new Block(0, "18/02/2024", "Genesis block","0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addblock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculaHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculaHash()){
+                return false
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false; 
+            }
+
+            return true;
+        }
+    }
+}
+
+let savjeeCoin = new Blockchain();
+savjeeCoin.addblock(new Block(1, "21/02/2024",{amount: 4}));
+savjeeCoin.addblock(new Block(2, "26/02/2024",{amount: 10}));
+
+//console.log(JSON.stringify(savjeeCoin, null ,4));
+
+console.log('se o bloco é valido ? ' + savjeeCoin.isChainValid());
+
+
+savjeeCoin.chain[1].data = {amout: 100};
+savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculaHash();
+
+
+
+
+console.log('se o bloco é valido ? ' + savjeeCoin.isChainValid());
